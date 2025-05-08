f0fc8a2ad41b6f3e75ec569cb14ec545130e585c
Kornchanok Iednusorn <kornchanokiednusorn@gmail.com>
2025-05-08 20:50:25 +0700

    improve structure

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,15 +5,15 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-export const environment = {
-  production: false,
-  firebase: { 
-    apiKey: "AIzaSyA28B943lDVqo210d6puhiVpo4vw4uvl_A",
-    authDomain: "angular-vertex-45747.firebaseapp.com",
-    projectId: "angular-vertex-45747",
-    storageBucket: "angular-vertex-45747.firebasestorage.app",
-    messagingSenderId: "819979609884",
-    appId: "1:819979609884:web:ba9feb92c6754940fbfcd9",
-    measurementId: "G-M9YCVZYT2X"
+export const environment = {  
+  production: false,  
+  firebase: {
+    apiKey: "example-api-key",
+    authDomain: "example-project.firebaseapp.com",
+    projectId: "example-project",
+    storageBucket: "example-project.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef1234567890",
+    measurementId: "G-EXAMPLE123"
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,13 @@
  */
 export const environment = {
   production: true,
-  firebase: {},
+  firebase: {
+    apiKey: "example-api-key",
+    authDomain: "example-project.firebaseapp.com",
+    projectId: "example-project",
+    storageBucket: "example-project.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef1234567890",
+    measurementId: "G-EXAMPLE123"
+  },
 };
